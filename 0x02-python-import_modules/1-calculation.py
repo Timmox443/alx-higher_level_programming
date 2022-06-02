3ce88ce24ccb991007a10ac8f17ae28f581f9b5b
Timmo2022 <timmomutumwa1@gmail.com>
2022-06-02 23:22:47 +0300

    added __name__

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-from calculator_1 import *
-a = 10
-b = 5
-print(f"{a} + {b} = {add(a, b)}\n{a} - {b} = {sub(a, b)}\n{a} * {b} = {mul(a, b)}\n{a} / {b} = {div(a, b)}") 
+if __name__ == "__main__":
+    from calculator_1 import *
+    a = 10
+    b = 5
+    q
+    print(f"{a} + {b} = {add(a, b)}\n{a} - {b} = {sub(a, b)}\n{a} * {b} = {mul(a, b)}\n{a} / {b} = {div(a, b)}") 
