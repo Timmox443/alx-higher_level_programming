79a4da2f7fb0478d4b73706bffbfdfad7ea162ea
Timmox443 <timmomutumwa1@gmail.com>
2022-09-07 01:26:10 +0300

    some

--- 0x12-javascript-warm_up/2-argument.js
+++ 0x12-javascript-warm_up/2-argument.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-
 const argC = process.argv.length;
-
 if (argC === 0){
 	console.log('No argument');
 } else if (argC === 1){
--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
 const argvOne = process.argv[2];
 if (argvOne === undefined) {
   console.log('No argument');
